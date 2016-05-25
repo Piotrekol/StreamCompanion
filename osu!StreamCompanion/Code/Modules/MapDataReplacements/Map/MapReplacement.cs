251c76601eaccb341200f89f9420aeea4d017326
Piotrekol <nara1011@gmail.com>
2016-05-25 13:12:03 +0200

    Add !OsuFileLocation! command
    Note that this is inaccurate when not playing or spectating

--- osu!StreamCompanion/Code/Modules/MapDataReplacements/Map/MapReplacement.cs
+++ osu!StreamCompanion/Code/Modules/MapDataReplacements/Map/MapReplacement.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections.Generic;
+using osu_StreamCompanion.Code.Core;
 using osu_StreamCompanion.Code.Core.DataTypes;
 using osu_StreamCompanion.Code.Helpers;
 using osu_StreamCompanion.Code.Interfeaces;
 
 namespace osu_StreamCompanion.Code.Modules.MapDataReplacements.Map
 {
-    class MapReplacement :IModule,IMapDataReplacements
+    class MapReplacement :IModule,IMapDataReplacements,ISettings
     {
+        private Settings _settings;
         public bool Started { get; set; }
         public void Start(ILogger logger){ Started = true; }
 
@@ -15,10 +17,22 @@ namespace osu_StreamCompanion.Code.Modules.MapDataReplacements.Map
             if (map.FoundBeatmaps)
             {
                 var dict = map.BeatmapsFound[0].GetDict(map.Mods);
+
+                var osuLocation = _settings.Get("MainOsuDirectory", "");
+                if (string.IsNullOrWhiteSpace(osuLocation))
+                    dict.Add("!OsuFileLocation!","");
+                else
+                    dict.Add("!OsuFileLocation!",System.IO.Path.Combine(osuLocation, "Songs", map.BeatmapsFound[0].Dir, map.BeatmapsFound[0].OsuFileName));
+
                 return dict;
             }
 
             return new Dictionary<string, string>();
+        }
+
+        public void SetSettingsHandle(Settings settings)
+        {
+            _settings = settings;
         }
     }
 }
