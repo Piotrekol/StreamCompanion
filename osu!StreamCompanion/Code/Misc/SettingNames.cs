734f2ee08e1835a812a69eabbd83f1710c717159
Piotr <nara1011@gmail.com>
2016-06-07 18:52:57 +0200

    Implement singleton class for storing setting names

---
+++ osu!StreamCompanion/Code/Misc/SettingNames.cs
@@ -0,0 +1,31 @@
+﻿namespace osu_StreamCompanion.Code.Misc
+{
+
+    public sealed class SettingNames
+    {
+        public readonly ConfigEntry setting1 = new ConfigEntry("settingname", value:false);
+        
+        static readonly SettingNames _instance = new SettingNames();
+        public static SettingNames Instance
+        {
+            get { return _instance; }
+        }
+
+        private SettingNames()
+        { }
+    }
+    public class ConfigEntry
+    {
+        public ConfigEntry(string name, object value)
+        {
+            Name = name;
+            _defaultValue = value;
+        }
+        public string Name { get; set; }
+        private readonly object _defaultValue;
+        public T Default<T>()
+        {
+            return (T)_defaultValue;
+        }
+    }
+}
