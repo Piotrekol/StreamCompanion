1f879bdb8f3148d19c295b1db00fe327c011390b
Piotr <nara1011@gmail.com>
2016-06-07 18:23:46 +0200

    Add textbox/label usercontrol "field"

---
+++ osu!StreamCompanion/Code/Misc/Field.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace osu_StreamCompanion.Code.Misc
+{
+    partial class Field
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(4, 4);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(35, 13);
+            this.label.TabIndex = 0;
+            this.label.Text = "label1";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(84, 1);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(170, 20);
+            this.textBox.TabIndex = 1;
+            // 
+            // Field
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label);
+            this.Name = "Field";
+            this.Size = new System.Drawing.Size(373, 25);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label label;
+        public System.Windows.Forms.TextBox textBox;
+    }
+}
---
+++ osu!StreamCompanion/Code/Misc/Field.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace osu_StreamCompanion.Code.Misc
+{
+    public partial class Field : UserControl
+    {
+        private string _labelText;
+        private bool _isPassword;
+
+        public string LabelText
+        {
+            get { return _labelText; }
+            set
+            {
+                label.Text = value;
+                textBox.Location = new Point(label.Size.Width+5, textBox.Location.Y);
+                _labelText = value;
+            }
+        }
+
+        public bool IsPassword
+        {
+            get { return _isPassword; }
+            set
+            {
+                textBox.PasswordChar = value ? '*' : '\0';
+                _isPassword = value;
+            }
+        }
+
+        public Field()
+        {
+            Start("Default text", false);
+        }
+        public Field(string text, bool isPassword)
+        {
+            Start(text, isPassword);
+        }
+
+        protected void Start(string text, bool isPassword)
+        {
+            InitializeComponent();
+
+            AutoSize = true;
+            Text = text;
+            label.AutoSize = true;
+            label.Anchor = AnchorStyles.Left;
+            label.TextAlign = ContentAlignment.MiddleLeft;
+            textBox.Anchor = AnchorStyles.Left;
+
+            this.IsPassword = isPassword;
+        }
+
+    }
+}
