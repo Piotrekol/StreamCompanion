1d5bd11e3b53e08209205ad0f4076574f3a95623
Piotrekol <nara1011@gmail.com>
2018-05-04 17:33:07 +0200

    Add support for sending ASCII characters in FileMapManager

--- osu!StreamCompanion/Code/Modules/MapDataGetters/FileMap/FileMapManager.cs
+++ osu!StreamCompanion/Code/Modules/MapDataGetters/FileMap/FileMapManager.cs
@@ -12,23 +12,27 @@ namespace osu_StreamCompanion.Code.Modules.MapDataGetters.FileMap
         private class MapContainer
         {
             public MemoryMappedFile File { get; set; }
+            public bool ASCIIonly = false;
             private readonly object _lockingObject = new object();
-            
+
             public void Write(string data)
             {
                 lock (_lockingObject)
                 {
-                    var bytes = Encoding.Unicode.GetBytes(data);
+                    byte[] bytes;
+                    if (ASCIIonly)
+                        bytes = Encoding.ASCII.GetBytes(data.Replace("\\n","\n"));
+                    else
+                        bytes = Encoding.Unicode.GetBytes(data);
                     using (var a = File.CreateViewStream())
                     {
-
                         a.Write(bytes, 0, bytes.Length);
                         a.WriteByte(0);
                     }
                 }
             }
         }
-        
+
         private MapContainer GetFile(string pipeName)
         {
             lock (_lockingObject)
