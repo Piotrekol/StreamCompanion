9cda3f7b9d081619994cff59b5f055778b448111
Piotrekol <nara1011@gmail.com>
2018-06-27 17:45:02 +0200

    IPlugin comments

--- StreamCompanionTypes/Interfaces/IPlugin.cs
+++ StreamCompanionTypes/Interfaces/IPlugin.cs
@@ -2,7 +2,9 @@
 {
     /// <summary>
     /// Defines a class that should get initalized in StreamCompanion <para/>
-    /// All plugins are required to implement <see cref="IPlugin"/> in order to get loaded
+    /// All plugins are required to implement <see cref="IPlugin"/> in order to get loaded <para/>
+    /// There is NO guaranted order of with handlers for other interfaces(like <see cref="ISettingsGetter"/> or <see cref="ISqliteUser"/>) will get called <para/>
+    /// Only guarantee is that <see cref="IModule.Start"/> (with <see cref="IPlugin"/> implements) will get called as last
     /// </summary>
     public interface IPlugin : IModule
     {
