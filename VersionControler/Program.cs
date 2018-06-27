33238792a1578b59ab20bbd9cea1b0c0dc5bcb24
Piotrekol <nara1011@gmail.com>
2018-06-27 17:44:47 +0200

    Fix for the final time problems with compiling SC after pulling
    doesn't depend on given line number anymore

--- VersionControler/Program.cs
+++ VersionControler/Program.cs
@@ -8,14 +8,15 @@ namespace VersionControler
     {
         private static string[] usage = new[]
         {
-            "VersionControler.exe FileToEdit lineNumber [replacementPattern]",
+            "VersionControler.exe FileToEdit lineNumber [replacementPattern] [textToSearch]",
             "FileToEdit - Full path to file",
             "lineNumber - Line number to edit, first line being 0 ",
-            "[replacementPattern] - string.Format target ({0} being version string) "
+            "[replacementPattern] - string.Format target ({0} being version string) ",
+            "[textToSearch] - if lineNumber is -1 this is used to find line containing this text"
         };
         /// <summary>
         /// Usage:
-        /// VersionControler.exe FileToEdit lineNumber [replacementPattern]
+        /// VersionControler.exe FileToEdit lineNumber [replacementPattern] [text to search]
         /// FileToEdit - Full path to file
         /// lineNumber - Line number to edit, first line being 0 
         /// </summary>
@@ -28,7 +29,7 @@ namespace VersionControler
             }
             string replacementPattern = "{0}=\"v{1}\";";
             bool custom = false;
-            if (args.Length == 3)
+            if (args.Length >= 3 && !string.IsNullOrWhiteSpace(args[2]))
             {
                 replacementPattern = args[2];
                 custom = true;
@@ -39,6 +40,27 @@ namespace VersionControler
             if (File.Exists(filePath))
             {
                 String[] lines = File.ReadAllLines(filePath);
+
+                if (lineNumber == -1)
+                {
+                    if (args.Length < 4)
+                    {
+                        Console.WriteLine("No line or text to find provided");
+                        return;
+                    }
+
+                    lineNumber = lines.Select((value, index) => new { value, index = index + 1 })
+                                    .Where(pair => pair.value.Contains(args[3]))
+                                    .Select(pair => pair.index)
+                                    .FirstOrDefault() - 1;
+                    if (lineNumber < 0)
+                    {
+                        Console.WriteLine("Could not find line to replace");
+                        return;
+                    }
+
+                }
+
                 DateTime d = DateTime.Now;
                 string version = d.ToString("yyMMdd.HH");
                 if (!custom)
