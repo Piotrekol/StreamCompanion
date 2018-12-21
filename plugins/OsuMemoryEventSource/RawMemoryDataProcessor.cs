5e6ed19a117569890dccc706a645f9942e68d770
Piotrekol <nara1011@gmail.com>
2018-12-21 22:59:55 +0100

    Fix:(temp) add logging around PPIfRestFCed

--- plugins/OsuMemoryEventSource/RawMemoryDataProcessor.cs
+++ plugins/OsuMemoryEventSource/RawMemoryDataProcessor.cs
@@ -4,6 +4,7 @@ using CollectionManager.DataTypes;
 using CollectionManager.Enums;
 using OppaiSharp;
 using OsuMemoryDataProvider;
+using StreamCompanionTypes.DataTypes;
 using Beatmap = StreamCompanionTypes.DataTypes.Beatmap;
 using Mods = OppaiSharp.Mods;
 
@@ -46,43 +47,65 @@ namespace OsuMemoryEventSource
             catch (FileNotFoundException) { }
         }
 
+        private bool PPIfRestFCedExceptionLogged = false;
         public double PPIfRestFCed()
         {
             if (_preprocessedBeatmap == null)
                 return double.NaN;
-            Accuracy accCalc;
-
-            PPv2 ppCalculator;
-            if (Play.Time <= 0)
+            try
             {
-                accCalc = new Accuracy(100d, _preprocessedBeatmap.Objects.Count, 0);
-                ppCalculator = new PPv2(new PPv2Parameters(_preprocessedBeatmap, accCalc.Count100,
-                    accCalc.Count50, accCalc.CountMiss, -1, accCalc.Count300, _currentMods));
+                Accuracy accCalc;
+
+                PPv2 ppCalculator;
+                if (Play.Time <= 0)
+                {
+                    accCalc = new Accuracy(100d, _preprocessedBeatmap.Objects.Count, 0);
+                    ppCalculator = new PPv2(new PPv2Parameters(_preprocessedBeatmap, accCalc.Count100,
+                        accCalc.Count50, accCalc.CountMiss, -1, accCalc.Count300, _currentMods));
+                    return ppCalculator.Total;
+                }
+
+                _preprocessedBeatmap.ResetCut();
+
+                //Calculate how much objects we can get starting from current Time
+                _preprocessedBeatmap.Cut(Play.Time, 100000000);
+                var c300Left = _preprocessedBeatmap.GetMaxComboSafe(true);
+                var comboLeft = _preprocessedBeatmap.GetMaxComboSafe();
+
+                var newMaxCombo = Math.Max(Play.MaxCombo, comboLeft + Play.Combo);
+                var newC300Count = Play.C300 + c300Left;
+
+                _preprocessedBeatmap.ResetCut();
+                if (newC300Count < 0)
+                    newC300Count = 0;
+
+                accCalc = new Accuracy(newC300Count, Play.C100, Play.C50, Play.CMiss);
+                _accIfRestFced = accCalc.Value() * 100;
+
+                ppCalculator = new PPv2(new PPv2Parameters(_preprocessedBeatmap, Play.C100,
+                    Play.C50, Play.CMiss, newMaxCombo, newC300Count, _currentMods));
                 return ppCalculator.Total;
             }
+            catch (ArgumentException e)
+            {
+                if (!PPIfRestFCedExceptionLogged)
+                {
+                    PPIfRestFCedExceptionLogged = true;
+                    e.Data.Add("playData", PlayDataToString(Play));
+                    e.Data.Add("mapID", _currentBeatmap?.MapId);
+                    e.Data.Add("mapHash", _currentBeatmap?.Md5);
+                    OsuMemoryEventSourceBase.Logger?.Log(e, LogLevel.Error);
+                }
+            }
 
-            _preprocessedBeatmap.ResetCut();
-
-            //Calculate how much objects we can get starting from current Time
-            _preprocessedBeatmap.Cut(Play.Time, 100000000);
-            var c300Left = _preprocessedBeatmap.GetMaxComboSafe(true);
-            var comboLeft = _preprocessedBeatmap.GetMaxComboSafe();
-
-            var newMaxCombo = Math.Max(Play.MaxCombo, comboLeft + Play.Combo);
-            var newC300Count = Play.C300 + c300Left;
-
-            _preprocessedBeatmap.ResetCut();
-            if (newC300Count < 0)
-                newC300Count = 0;
-
-            accCalc = new Accuracy(newC300Count, Play.C100, Play.C50, Play.CMiss);
-            _accIfRestFced = accCalc.Value() * 100;
-
-            ppCalculator = new PPv2(new PPv2Parameters(_preprocessedBeatmap, Play.C100,
-                    Play.C50, Play.CMiss, newMaxCombo, newC300Count, _currentMods));
-            return ppCalculator.Total;
+            return Double.NaN;
         }
-
+        private string PlayDataToString(PlayContainer p)
+        {
+            return $"{p.C300}/{p.C100}/{p.C50}/{p.CMiss}|"+
+                   $"acc:{p.Acc},combo: {p.Combo},maxCombo {p.MaxCombo}|" +
+                   $"time: {p.Time}";
+        }
         public double AimPPIfBeatmapWouldEndNow { get; private set; } = double.NaN;
         public double SpeedPPIfBeatmapWouldEndNow { get; private set; } = double.NaN;
         public double AccPPIfBeatmapWouldEndNow { get; private set; } = double.NaN;
