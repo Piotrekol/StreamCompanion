bc761f1f82310d1a83384f273361a67ccb530729
Piotrekol <nara1011@gmail.com>
2019-02-09 16:46:35 +0100

    BeatmapPpReplacements: OppaiSharp out / osu!lazer pp calc in

--- plugins/BeatmapPpReplacements/PpReplacements.cs
+++ plugins/BeatmapPpReplacements/PpReplacements.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
+﻿using CollectionManager.DataTypes;
 using CollectionManager.Enums;
-using OppaiSharp;
+using PpCalculator;
 using StreamCompanionTypes;
 using StreamCompanionTypes.DataTypes;
 using StreamCompanionTypes.Interfaces;
-using Beatmap = OppaiSharp.Beatmap;
-using Helpers = StreamCompanionTypes.Helpers;
+using System;
+using System.IO;
 
 namespace BeatmapPpReplacements
 {
@@ -16,11 +13,11 @@ namespace BeatmapPpReplacements
     {
         private readonly SettingNames _names = SettingNames.Instance;
 
-        DiffCalc diffCalculator = new DiffCalc();
-        PPv2 _ppCalculator;
-        Accuracy _accCalculator = new Accuracy();
+        //TODO: SC can now support all gamemodes
+        OsuCalculator _osuCalculator = new OsuCalculator();
+        
         private ISettingsHandler _settings;
-        private Mods _lastMods = Mods.NoMod;
+        private Mods _lastMods;
         private string _lastModsStr = "None";
         public bool Started { get; set; }
 
@@ -34,7 +31,7 @@ namespace BeatmapPpReplacements
         {
             Started = true;
         }
-        
+
         public Tokens GetMapReplacements(MapSearchResult map)
         {
             var ret = new Tokens
@@ -63,61 +60,60 @@ namespace BeatmapPpReplacements
             FileInfo file = new FileInfo(mapLocation);
 
             if (file.Length == 0) return ret;
-            Beatmap beatmap = null;
-            try
+
+            _osuCalculator.PreProcess(mapLocation);
+
+            ret["MaxCombo"] = new TokenWithFormat(_osuCalculator.GetMaxCombo());
+            ret["SSPP"] = new TokenWithFormat(GetPp(_osuCalculator, 100d), format: "{0:0.00}");
+            ret["99.9PP"] = new TokenWithFormat(GetPp(_osuCalculator, 99.9d), format: "{0:0.00}");
+            ret["99PP"] = new TokenWithFormat(GetPp(_osuCalculator, 99d), format: "{0:0.00}");
+            ret["98PP"] = new TokenWithFormat(GetPp(_osuCalculator, 98d), format: "{0:0.00}");
+            ret["95PP"] = new TokenWithFormat(GetPp(_osuCalculator, 95d), format: "{0:0.00}");
+            ret["90PP"] = new TokenWithFormat(GetPp(_osuCalculator, 90d), format: "{0:0.00}");
+
+            Mods mods;
+            string modsStr;
+            if (map.Action == OsuStatus.Playing || map.Action == OsuStatus.Watching)
             {
-                beatmap = BeatmapHelpers.GetOppaiSharpBeatmap(mapLocation);
-                if (beatmap == null)
-                    return ret;
-
-                ret["MaxCombo"] = new TokenWithFormat(beatmap.GetMaxCombo());
-
-                ret["SSPP"] = new TokenWithFormat(GetPp(beatmap, 100d));
-                ret["99.9PP"] = new TokenWithFormat(GetPp(beatmap, 99.9d));
-                ret["99PP"] = new TokenWithFormat(GetPp(beatmap, 99d));
-                ret["98PP"] = new TokenWithFormat(GetPp(beatmap, 98d));
-                ret["95PP"] = new TokenWithFormat(GetPp(beatmap, 95d));
-                ret["90PP"] = new TokenWithFormat(GetPp(beatmap, 90d));
-
-                Mods mods = _lastMods;
-                string modsStr = _lastModsStr;
-                if (map.Action == OsuStatus.Playing || map.Action == OsuStatus.Watching)
-                {
-                    var tempMods = (map.Mods?.Item1 ?? CollectionManager.DataTypes.Mods.Omod).Convert();
-                    if (tempMods != Mods.NoMod)
-                    {
-                        modsStr = map.Mods?.Item2 ?? "NM";
-                        mods = tempMods;
-
-                        _lastMods = tempMods;
-                        _lastModsStr = modsStr;
-                    }
-                }
-
-                ret["mMod"] = new Token(modsStr);
-                ret["mSSPP"] = new TokenWithFormat(GetPp(beatmap, 100d, mods));
-                ret["m99.9PP"] = new TokenWithFormat(GetPp(beatmap, 99.9d, mods));
-                ret["m99PP"] = new TokenWithFormat(GetPp(beatmap, 99d, mods));
-                ret["m98PP"] = new TokenWithFormat(GetPp(beatmap, 98d, mods));
-                ret["m95PP"] = new TokenWithFormat(GetPp(beatmap, 95d, mods));
-                ret["m90PP"] = new TokenWithFormat(GetPp(beatmap, 90d, mods));
-
-                return ret;
+                mods = map.Mods?.Item1 ?? Mods.Omod;
+                modsStr = map.Mods?.Item2 ?? "NM";
+                _lastMods = mods;
+                _lastModsStr = modsStr;
             }
-            catch
+            else
             {
+                mods = _lastMods;
+                modsStr = _lastModsStr;
             }
+
+            ret["mMod"] = new Token(modsStr);
+            ret["mSSPP"] = new TokenWithFormat(GetPp(_osuCalculator, 100d, mods), format: "{0:0.00}");
+            ret["m99.9PP"] = new TokenWithFormat(GetPp(_osuCalculator, 99.9d, mods), format: "{0:0.00}");
+            ret["m99PP"] = new TokenWithFormat(GetPp(_osuCalculator, 99d, mods), format: "{0:0.00}");
+            ret["m98PP"] = new TokenWithFormat(GetPp(_osuCalculator, 98d, mods), format: "{0:0.00}");
+            ret["m95PP"] = new TokenWithFormat(GetPp(_osuCalculator, 95d, mods), format: "{0:0.00}");
+            ret["m90PP"] = new TokenWithFormat(GetPp(_osuCalculator, 90d, mods), format: "{0:0.00}");
 
             return ret;
         }
-        private double GetPp(Beatmap beatmap, double acc, Mods mods = Mods.NoMod)
+        private double GetPp(PpCalculator.PpCalculator ppCalculator, double acc, Mods mods = Mods.Omod)
         {
-            _accCalculator = new Accuracy(acc, beatmap.Objects.Count, 0);
-            _ppCalculator = new PPv2(new PPv2Parameters(beatmap, _accCalculator.Count100, _accCalculator.Count50, _accCalculator.CountMiss, -1, _accCalculator.Count300, mods));
-            return Math.Round(_ppCalculator.Total, 2);
+            ppCalculator.Accuracy = acc;
+            
+            //TODO: (CollectionManager) autoplay and autopilot have invalid acronyms and scoreV2 does not exist in mods
+            ppCalculator.Mods = mods == Mods.Omod ? null : mods.ToString().Split(new[] { ", " }, StringSplitOptions.None);
+            try
+            {
+                return ppCalculator.Calculate();
+            }
+            catch (ArgumentException e)
+            {
+                if (!e.Message.Contains("Invalid mod provided"))
+                    throw;
+            }
+
+            return -1;
         }
-
-
         private string GetOsuDir()
         {
             return _settings.Get<string>(_names.MainOsuDirectory);
