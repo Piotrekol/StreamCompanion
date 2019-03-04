4896c8635eac3aeda57ed53cd4467f7078d86dc1
Piotrekol <nara1011@gmail.com>
2019-03-04 17:14:12 +0100

    Misc: Allow for changing bound Chart Fill prop name

--- plugins/LiveVisualizer/Chart.xaml.cs
+++ plugins/LiveVisualizer/Chart.xaml.cs
@@ -3,7 +3,11 @@ using LiveCharts.Helpers;
 using System;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Windows.Controls;
+using System.Windows.Data;
+using LiveCharts.Wpf;
+using LiveVisualizer.Annotations;
 
 namespace LiveVisualizer
 {
@@ -12,9 +16,14 @@ namespace LiveVisualizer
     /// </summary>
     public partial class Chart : UserControl
     {
-        public Chart(IWpfVisualizerData data)
+        public Chart(IWpfVisualizerData data, string bindingName)
         {
             InitializeComponent();
+
+            var binding = new Binding(bindingName);
+            binding.Source = data;
+            LineSeries.SetBinding(Series.FillProperty, binding);
+
             DataContext = data;
         }
     }
