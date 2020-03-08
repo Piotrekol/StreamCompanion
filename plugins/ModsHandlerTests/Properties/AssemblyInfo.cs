087eff950b7009bedb57f9da462f601d762bdde6
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2020-03-08 19:17:24 +0100

    update tests

--- plugins/ModsHandlerTests/Properties/AssemblyInfo.cs
+++ plugins/ModsHandlerTests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("44b0dab1-55a5-45dd-8209-093e8f517466")]
+[assembly: Guid("1fcfbd79-804a-45bc-9433-c3f4b554a0af")]
 
 // Version information for an assembly consists of the following four values:
 //
