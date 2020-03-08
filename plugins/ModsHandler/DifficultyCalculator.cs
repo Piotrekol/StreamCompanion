7f06cf9cdbbe795e86350cb6201f47c1c4590244
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2020-03-08 21:31:03 +0100

    Fix: Incorrect modded values for map OD (!mOD!) (#144)

--- plugins/ModsHandler/DifficultyCalculator.cs
+++ plugins/ModsHandler/DifficultyCalculator.cs
@@ -7,11 +7,14 @@ namespace ModsHandler
 {
     public class DifficultyCalculator
     {
-        readonly float ar0_ms = 1800f,
+        readonly float od0_ms = 79.5f,
+           od10_ms = 19.5f,
+           ar0_ms = 1800f,
            ar5_ms = 1200f,
            ar10_ms = 450f;
 
-        readonly float ar_ms_step1 = 120, // ar0-5 
+        readonly float od_ms_step = 6,
+            ar_ms_step1 = 120, // ar0-5 
             ar_ms_step2 = 150; // ar5-10 
 
         public Dictionary<string, float> ApplyMods(IBeatmap map, Mods mods)
@@ -40,6 +43,17 @@ namespace ModsHandler
                 speed *= 1.5f;
             if ((mods & Mods.Ht) != 0)
                 speed *= 0.75f;
+
+            float od_multiplier = 1;
+            if ((mods & Mods.Hr) != 0)
+                od_multiplier *= 1.4f;
+            if ((mods & Mods.Ez) != 0)
+                od_multiplier *= 0.5f;
+
+            od *= od_multiplier;
+            float odms = (mods & Mods.Hr) != 0 && (mods & Mods.Dt) == 0 && (mods & Mods.Ht) == 0
+                ? od0_ms - (od_ms_step * od)
+                : od0_ms - (float)Math.Ceiling(od_ms_step * od);
 
             //hp
             if ((mods & Mods.Ez) != 0)
@@ -82,25 +96,23 @@ namespace ModsHandler
                 cs_multiplier *= 0.5f;
 
             // stats must be capped to 0-10 before HT/DT which bring them to a range 
-            // of -5 to 11 for AR 
+            // of -4.42 to 11.08 for OD and -5 to 11 for AR 
+            odms = Math.Min(od0_ms, Math.Max(od10_ms, odms));
             arms = Math.Min(ar0_ms, Math.Max(ar10_ms, arms));
 
             // apply speed-changing mods 
+            odms /= speed;
             arms /= speed;
 
-            // convert AR back into their stat form 
+            // convert OD and AR back into their stat form 
+            //od = (-(odms - od0_ms)) / od_ms_step; 
+            od = (od0_ms - odms) / od_ms_step;
             ar = ar <= 5.0f
                 ? ((ar0_ms - arms) / ar_ms_step1)
                 : (5.0f + (ar5_ms - arms) / ar_ms_step2);
 
             cs *= cs_multiplier;
             cs = Math.Max(0.0f, Math.Min(10.0f, cs));
-
-            var isDtPlay = (mods & Mods.Dt) != 0 || (mods & Mods.Nc) != 0;
-            if ((mods & Mods.Ez) != 0)
-                od = Math.Max(0, od / 2);
-            else
-                od = Math.Min(isDtPlay ? 11.08f : 10f, od * 1.4f);
 
             retValue.Add("AR", ar);
             retValue.Add("CS", cs);
---
+++ plugins/ModsHandlerTests/DifficultyCalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using CollectionManager.DataTypes;
+using ModsHandler;
+using NUnit.Framework;
+using Beatmap = StreamCompanionTypes.DataTypes.Beatmap;
+
+namespace ModsHandlerTests
+{
+    [TestFixture()]
+    public class DifficultyCalculatorTests
+    {
+        private Dictionary<Mods, List<float>> OD_VALUES = new Dictionary<Mods, List<float>>
+        {
+            {Mods.Omod,  new List<float>{10f, 9f, 8f, 7f, 6f, 5f, 4f, 3f, 2f, 1f, 0} },
+            {Mods.Dt,  new List<float>{11.08f, 10.42f, 9.75f, 9.08f, 8.42f, 7.75f, 7.08f, 6.42f, 5.75f, 5.08f, 4.42f} },
+            {Mods.Ht,  new List<float>{8.92f, 7.54f, 6.25f, 4.92f, 3.54f, 2.25f, 0.92f, -0.42f, -1.75f, -3.08f, -4.42f} },
+            {Mods.Hr,  new List<float>{10f, 10f, 10f, 9.8f, 8.4f, 7f, 5.6f, 4.2f, 2.8f, 1.4f, 0} },
+            {Mods.Hr | Mods.Dt,  new List<float>{11.08f, 11.08f, 11.08f, 10.97f, 10.08f, 9.08f, 8.19f, 7.31f, 6.31f, 5.42f, 4.42f} },
+            {Mods.Hr | Mods.Ht,  new List<float>{8.92f, 8.92f, 8.92f, 8.69f, 6.92f, 4.92f, 3.14f, 1.36f, -0.64f, -2.42f, -4.42f} },
+            {Mods.Ez,  new List<float>{5f, 4.5f, 4f, 3.5f, 3f, 2.5f, 2f, 1.5f, 1f, 0.5f, 0} },
+            {Mods.Ez | Mods.Dt,  new List<float>{7.75f, 7.42f, 7.08f, 6.75f, 6.42f, 6.08f, 5.75f, 5.42f, 5.08f, 4.75f, 4.42f} },
+            {Mods.Ez | Mods.Ht,  new List<float>{2.25f, 1.54f, 0.92f, 0.25f, 0.42f, -1.08f, -1.75f, -2.42f, -3.08f, -3.75f, -4.42f} },
+        };
+
+        [Test]
+        [TestCase(Mods.Omod)]
+        [TestCase(Mods.Ht, Ignore = "value mismatch in a range of 0.1f")]
+        [TestCase(Mods.Hr)]
+        [TestCase(Mods.Hr | Mods.Dt)]
+        [TestCase(Mods.Hr | Mods.Ht)]
+        [TestCase(Mods.Ez)]
+        [TestCase(Mods.Ez | Mods.Dt)]
+        [TestCase(Mods.Ez | Mods.Ht, Ignore = "value mismatch in a range of 0.1f")]
+        public void TestOD(Mods mods)
+        {
+            var diffCalc = new DifficultyCalculator();
+            var beatmap = new Beatmap();
+
+            var resultValues = new List<float>();
+            for (var i = 10; i >= 0; i--)
+            {
+                beatmap.OverallDifficulty = i;
+                var odResult = diffCalc.ApplyMods(beatmap, mods)["OD"];
+                resultValues.Add((float)Math.Round(odResult, 2));
+            }
+            CollectionAssert.AreEqual(OD_VALUES[mods], resultValues);
+        }
+    }
+}
