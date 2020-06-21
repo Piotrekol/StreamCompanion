ea046bec26a72e15370fc0bafb579f96d3b98349
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2020-06-21 22:40:08 +0200

    Migrated MemoryEventSource

--- plugins/OsuMemoryEventSource/LivePerformanceCalculator.cs
+++ plugins/OsuMemoryEventSource/LivePerformanceCalculator.cs
@@ -4,7 +4,6 @@ using PpCalculator;
 using System;
 using System.Collections.Generic;
 using StreamCompanionTypes.DataTypes;
-using Beatmap = StreamCompanionTypes.DataTypes.Beatmap;
 
 namespace OsuMemoryEventSource
 {
