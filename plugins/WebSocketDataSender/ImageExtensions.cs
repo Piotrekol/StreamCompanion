42080c3d924be2ba25b8a11321365789bd769167
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2020-08-30 14:03:48 +0200

    move ResizeImage to extension class

---
+++ plugins/WebSocketDataSender/ImageExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace WebSocketDataSender
+{
+    public static class ImageExtensions
+    {
+        /// <summary>
+        /// Resize the image to the specified width and height, while keeping aspect ratio.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(this Image image, int? width, int? height)
+        {
+            if ((!width.HasValue || width == 0) && (!height.HasValue || height == 0))
+                throw new ArgumentNullException(nameof(width), "Both width and height cannot be null. Provide value for at least one of them.");
+
+            double ratioX = (double)(width ?? double.MaxValue) / (double)image.Width;
+            double ratioY = (double)(height ?? double.MaxValue) / (double)image.Height;
+            // use whichever multiplier is smaller
+            double ratio = ratioX < ratioY ? ratioX : ratioY;
+
+            // now we can get the new height and width
+            int newHeight = Convert.ToInt32(image.Height * ratio);
+            int newWidth = Convert.ToInt32(image.Width * ratio);
+
+            var destRect = new Rectangle(0, 0, newWidth, newHeight);
+            var destImage = new Bitmap(newWidth, newHeight);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
--- plugins/WebSocketDataSender/WebSocketDataGetter.cs
+++ plugins/WebSocketDataSender/WebSocketDataGetter.cs
@@ -98,8 +98,8 @@ namespace WebSocketDataSender
 
                                 using (var img = Image.FromStream(fs))
                                 {
-                                    using (var resizedImg = ResizeImage(img, 
-                                        desiredWidth == 0 ? (int?)null : desiredWidth, 
+                                    using (var resizedImg = img.ResizeImage(
+                                        desiredWidth == 0 ? (int?)null : desiredWidth,
                                         desiredHeight == 0 ? (int?)null : desiredHeight)
                                     )
                                     {
@@ -116,49 +116,6 @@ namespace WebSocketDataSender
                     }
                 }
             }
-        }
-        /// <summary>
-        /// Resize the image to the specified width and height.
-        /// </summary>
-        /// <param name="image">The image to resize.</param>
-        /// <param name="width">The width to resize to.</param>
-        /// <param name="height">The height to resize to.</param>
-        /// <returns>The resized image.</returns>
-        public static Bitmap ResizeImage(Image image, int? width, int? height)
-        {
-            if((!width.HasValue || width == 0) && (!height.HasValue || height == 0))
-                throw new ArgumentNullException("width","Both width and height cannot be null. Provide value for at least one of them.");
-                
-            double ratioX = (double)(width ?? double.MaxValue)/ (double)image.Width;
-            double ratioY = (double)(height ?? double.MaxValue) / (double)image.Height;
-            // use whichever multiplier is smaller
-            double ratio = ratioX < ratioY ? ratioX : ratioY;
-
-            // now we can get the new height and width
-            int newHeight = Convert.ToInt32(image.Height * ratio);
-            int newWidth = Convert.ToInt32(image.Width * ratio);
-
-            var destRect = new Rectangle(0, 0, newWidth, newHeight);
-            var destImage = new Bitmap(newWidth, newHeight);
-
-            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-            using (var graphics = Graphics.FromImage(destImage))
-            {
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-
-                using (var wrapMode = new ImageAttributes())
-                {
-                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-                }
-            }
-
-            return destImage;
         }
 
         public void Dispose()
