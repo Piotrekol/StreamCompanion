880ed4f27e213e46a413e736afd005fc2d19fb1e
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2020-10-01 21:45:23 +0200

    Misc: Update logo

--- plugins/ScGui/AboutForm.Designer.cs
+++ plugins/ScGui/AboutForm.Designer.cs
@@ -49,7 +49,6 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = null;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(64, 64);
