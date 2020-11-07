a4b85b58368e6fadaa123169920eca5271d168a1
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2020-11-07 11:25:52 +0100

    Misc: Create helper object for converting grade token values

--- webOverlay/lib/consts.js
+++ webOverlay/lib/consts.js
@@ -1,20 +1,35 @@
 let config = {
-    scheme: window.location.protocol.slice(0, -1),
-    host: window.location.hostname,
-    port: window.location.port,
-    getUrl: () => `${config.scheme}://${config.host}:${config.port}`,
-    getWs: () => `ws://${config.host}:${config.port}`,
-}
+  scheme: window.location.protocol.slice(0, -1),
+  host: window.location.hostname,
+  port: window.location.port,
+  getUrl: () => `${config.scheme}://${config.host}:${config.port}`,
+  getWs: () => `ws://${config.host}:${config.port}`,
+};
 
-osuStatus = Object.freeze({
-    Null: 0,
-    Listening: 1,
-    Playing: 2,
-    Watching: 8,
-    Editing: 16,
-    ResultsScreen: 32
-})
+osuStatus = {
+  Null: 0,
+  Listening: 1,
+  Playing: 2,
+  Watching: 8,
+  Editing: 16,
+  ResultsScreen: 32,
+};
 
-window.overlay = Object.freeze({
-    osuStatus, config
-})
+osuGrade = {
+  0: 'SSH',
+  1: 'SH',
+  2: 'SS',
+  3: 'S',
+  4: 'A',
+  5: 'B',
+  6: 'C',
+  7: 'D',
+  8: 'F',
+  9: '',
+};
+
+window.overlay = {
+  osuStatus,
+  osuGrade,
+  config,
+};
