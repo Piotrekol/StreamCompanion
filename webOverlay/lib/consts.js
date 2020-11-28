c5b2c9549264096e1fac7916c041df9f581acbba
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2020-11-28 11:32:36 +0100

    Misc: Create helper object for rawStatus token conversion
    #191

--- webOverlay/lib/consts.js
+++ webOverlay/lib/consts.js
@@ -6,7 +6,7 @@ let config = {
   getWs: () => `ws://${config.host}:${config.port}`,
 };
 
-osuStatus = {
+let osuStatus = {
   Null: 0,
   Listening: 1,
   Playing: 2,
@@ -15,7 +15,29 @@ osuStatus = {
   ResultsScreen: 32,
 };
 
-osuGrade = {
+let rawOsuStatus = {
+  Unknown: -2,
+  NotRunning: -1,
+  MainMenu: 0,
+  EditingMap: 1,
+  Playing: 2,
+  GameShutdownAnimation: 3,
+  SongSelectEdit: 4,
+  SongSelect: 5,
+  ResultsScreen: 7,
+  GameStartupAnimation: 10,
+  MultiplayerRooms: 11,
+  MultiplayerRoom: 12,
+  MultiplayerSongSelect: 13,
+  MultiplayerResultsscreen: 14,
+  OsuDirect: 15,
+  RankingTagCoop: 17,
+  RankingTeam: 18,
+  ProcessingBeatmaps: 19,
+  Tourney: 22,
+}
+
+let osuGrade = {
   0: 'SSH',
   1: 'SH',
   2: 'SS',
@@ -30,6 +52,7 @@ osuGrade = {
 
 window.overlay = {
   osuStatus,
+  rawOsuStatus,
   osuGrade,
   config,
 };
