ef6038161e496a7e419120c95c182d88e1279ced
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2021-10-03 10:56:57 +0200

    Misc: Log contextData with exceptions
    Code should be now aligned with what is in dev docs

--- osu!StreamCompanion/Code/Core/Loggers/ConsoleLogger.cs
+++ osu!StreamCompanion/Code/Core/Loggers/ConsoleLogger.cs
@@ -1,5 +1,6 @@
 using System;
 using System.CodeDom.Compiler;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.Versioning;
@@ -13,11 +14,13 @@ using StreamCompanionTypes.Interfaces.Services;
 namespace osu_StreamCompanion.Code.Core.Loggers
 {
     [SupportedOSPlatform("windows")]
-    class ConsoleLogger : ILogger, IDisposable
+    class ConsoleLogger : IContextAwareLogger, IDisposable
     {
         private readonly SettingNames _names = SettingNames.Instance;
 
         private readonly ISettings _settings;
+        public static Dictionary<string, string> ContextData { get; } = new();
+        private object _lockingObject = new();
 
         public ConsoleLogger(ISettings settings)
         {
@@ -25,7 +28,7 @@ namespace osu_StreamCompanion.Code.Core.Loggers
             NativeMethods.AllocConsole();
             Console.Title = "StreamCompanion logs";
             Console.SetOut(TextWriter.Synchronized(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true }));
-            
+
             if (Console.LargestWindowWidth > 0)
                 Console.WindowWidth = Console.LargestWindowWidth - Convert.ToInt32(Console.LargestWindowWidth / 3);
         }
@@ -39,7 +42,21 @@ namespace osu_StreamCompanion.Code.Core.Loggers
         public void Log(object logMessage, LogLevel loglvevel, params string[] vals)
         {
             if (_settings.Get<int>(_names.LogLevel) <= loglvevel.GetHashCode())
-                Console.WriteLine(logMessage.ToString());
+            {
+                if (logMessage is Exception)
+                {
+                    lock (_lockingObject)
+                        Console.WriteLine(logMessage + Environment.NewLine + string.Join(Environment.NewLine, ContextData));
+                }
+                else
+                    Console.WriteLine(logMessage);
+            }
+        }
+
+        public void SetContextData(string key, string value)
+        {
+            lock (_lockingObject)
+                ContextData[key] = value;
         }
     }
 }
