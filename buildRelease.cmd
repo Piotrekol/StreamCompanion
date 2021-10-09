20fc9333346b814700faa1732aa7985fa728d141
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2021-10-09 19:01:20 +0200

    Misc: Adjust local build script to use AnyCPU builds

--- buildRelease.cmd
+++ buildRelease.cmd
@@ -7,7 +7,7 @@ mkdir .\build\Output
 
 build\nuget.exe restore
 REM build solution
-build\msbuild.bat . /p:Configuration=Release /p:Platform=x86
+build\msbuild.bat . /p:Configuration=Release /p:Platform="Any CPU"
 
 REM copy web overlays
 robocopy .\webOverlay .\build\Release\Files\Web /E
