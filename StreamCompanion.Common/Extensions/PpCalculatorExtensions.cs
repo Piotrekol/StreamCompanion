a03ecc7ee8d8b376814a574a005d6dc36a10ffa3
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2023-03-18 14:03:44 +0100

    Fix: Use hit object density instead of strains for Mania

--- StreamCompanion.Common/Extensions/PpCalculatorExtensions.cs
+++ StreamCompanion.Common/Extensions/PpCalculatorExtensions.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Threading;
+using CollectionManager.Enums;
 using PpCalculatorTypes;
 
 namespace StreamCompanion.Common
@@ -11,23 +11,17 @@ namespace StreamCompanion.Common
         public static Dictionary<int, double> CalculateStrains(this IPpCalculator ppCalculator,
             CancellationToken cancellationToken, int? targetAmount = null)
         {
+            if (ppCalculator.RulesetId == (int)PlayMode.OsuMania)
+                return CalculateHitObjectDensity(ppCalculator, cancellationToken, targetAmount);
+
             var strains = new Dictionary<int, double>(targetAmount ?? 200);
             if (ppCalculator == null)
                 return strains;
 
             ppCalculator.Score = 1_000_000;
             var mapLength = ppCalculator.BeatmapLength;
-            //data for 2min map
-            var strainLength = 5000;
-            var interval = 1500;
+            var (interval, strainLength) = CalculateStrainParameters(ppCalculator, targetAmount);
             var time = 0;
-
-            if (targetAmount.HasValue)
-            {
-                strainLength = Convert.ToInt32(Math.Floor(strainLength * (mapLength / 120_000d)));
-                interval = Convert.ToInt32(Math.Ceiling(mapLength - strainLength / 2d) / targetAmount) + 1;
-                //var strainsAmount = (mapLength - strainLength / 2d) / interval;
-            }
 
             while (time + strainLength / 2 < mapLength)
             {
@@ -43,6 +37,41 @@ namespace StreamCompanion.Common
             }
 
             return strains;
+        }
+
+        public static Dictionary<int, double> CalculateHitObjectDensity(this IPpCalculator ppCalculator,
+            CancellationToken cancellationToken, int? targetAmount = null)
+        {
+            if (ppCalculator == null)
+                return null;
+
+            var strains = new Dictionary<int, double>(targetAmount ?? 200);
+            var (interval, _) = CalculateStrainParameters(ppCalculator, targetAmount);
+            var graphValues = ppCalculator.CalculateProgressGraphValues(cancellationToken, targetAmount ?? 200);
+            var time = 0;
+            foreach (var graphValue in graphValues)
+            {
+                strains[time] = graphValue;
+                time += interval;
+            }
+
+            return strains;
+        }
+
+        private static (int Interval, int StrainLength) CalculateStrainParameters(IPpCalculator ppCalculator, int? targetAmount = null)
+        {
+            var mapLength = ppCalculator.BeatmapLength;
+            //data for 2min map
+            var strainLength = 5000;
+            var interval = 1500;
+
+            if (targetAmount.HasValue)
+            {
+                strainLength = Convert.ToInt32(Math.Floor(strainLength * (mapLength / 120_000d)));
+                interval = Convert.ToInt32(Math.Ceiling(mapLength - (strainLength / 2d)) / targetAmount) + 1;
+            }
+
+            return (interval, strainLength);
         }
     }
 }
