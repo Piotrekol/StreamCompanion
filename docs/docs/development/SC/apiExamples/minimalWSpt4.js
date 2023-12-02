ff189fd6eef2ae4ae4f44ae73c67af8114f6aa33
Piotrekol <4990365+Piotrekol@users.noreply.github.com>
2023-12-02 11:47:26 +0100

    Docs: WebSocket /tokens throttling

---
+++ docs/docs/development/SC/apiExamples/minimalWSpt4.js
@@ -0,0 +1,10 @@
+let ws = new WebSocket('ws://localhost:20727/tokens?bulkUpdates=MainPipeline,LiveTokens&updatesPerSecond=15');
+ws.onopen = () => {
+  ws.send(JSON.stringify(['rankedStatus', 'titleRoman', 'artistUnicode']));
+};
+let cache = {};
+ws.onmessage = (wsEvent) => {
+  Object.assign(cache, JSON.parse(wsEvent.data));
+  //cache will always contain up to date token values
+  console.log(cache);
+};
